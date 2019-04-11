d5ce7e2562211fdcfd9d146bfbda5297b50398a5
彬 <17661977890@163.com>
2019-04-11 17:05:10 +0800

    新增spring-cloud-config 配置中心项目

---
+++ src/main/java/com/example/vosungconfigserver/VosungConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vosungconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VosungConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VosungConfigServerApplication.class, args);
+	}
+
+}
