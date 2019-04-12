8d752fec65d40dc60127e4210c7cc4d485d0b624
彬 <17661977890@163.com>
2019-04-12 16:02:59 +0800

    向eureka注册为微服务

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ config-server：
 http://localhost:7001/didspace/dev/master
 
 {application}/{profile}/git分支名称
+
+控制台输出信息:
+o.s.c.c.s.e.NativeEnvironmentRepository  : Adding property source: file:/C:/Users/彬/AppData/Local/Temp/config-repo-3629415525264078477/configfile/config-repo/didispace-test.properties
+配置服务器会从git中获取配置信息后，会存储一份在config-server的文件系统中，实际上就是通过git clone
+命令将配置内容复制一份在本地存储，然后读取这些内容返回给微服务应用。类似于缓存一样，如果git仓库出现问题，
+可以加在本地的缓存的配置信息。
+
+
+
+增加eureka，实现微服务注册的配置中心
--- src/main/java/com/example/vosungconfigserver/VosungConfigServerApplication.java
+++ src/main/java/com/example/vosungconfigserver/VosungConfigServerApplication.java
@@ -2,11 +2,13 @@ package com.example.vosungconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @EnableConfigServer 开启spring cloud config 得服务端功能
  */
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class VosungConfigServerApplication {
